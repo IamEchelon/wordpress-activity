682b0245e369b789361193b216b6be7e429d8672
Brent Brinkley <brentbrinkley@gmail.com>
2016-02-01 08:41:53 -0500

    initial layout fix

---
+++ wp-content/themes/wordpress-starter-theme/front-page.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+
+<?php $query = new WP_Query( array(
+                'post_type' => 'post',
+                'posts_per_page' => 2
+            ) );
+            while ($query->have_posts()) : $query->the_post(); ?>
+            <li class="home update"><a href="<?php the_permalink(); ?>"><?php the_title(); ?> - <?php the_date( 'j F' ); ?></a></li>
+            <?php endwhile; ?>
+
+
+
+<?php get_footer(); ?>
--- wp-content/themes/wordpress-starter-theme/index.php
+++ wp-content/themes/wordpress-starter-theme/index.php
@@ -6,7 +6,8 @@
 <?php while ( have_posts() ) : the_post(); ?>
 
 	<!-- Page Structure on feed -->
-	
+	<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+	<p><?php the_content(); ?></p>
 
 <?php endwhile; ?>
 
