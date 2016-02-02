d3da93680243496e1031fec4ca0b36a7d7a5e2a0
Brent Brinkley <brentbrinkley@gmail.com>
2016-02-02 09:51:34 -0500

    more css updates

--- wp-content/themes/wordpress-starter-theme/sidebar.php
+++ wp-content/themes/wordpress-starter-theme/sidebar.php
@@ -1,23 +1,24 @@
-
-<h2>Upcoming Events</h2>
-<?php $query = new WP_Query( array(
-	                'post_type' => 'post',
-	                'category_name' => 'event',
-	                'order_by' => 'meta_value_num',
-	                'meta_key'  => 'event_date_info',
-	                'posts_per_page' => 3
-	            ) );
-	            
-	          
-	            while ($query->have_posts()) : $query->the_post(); ?>
-		            <div class="home-update">
-			           
-			            <a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
-			            <h4><?php echo get_post_meta(get_the_ID(), 'event_date_info', true); ?>
-			            - <?php echo get_post_meta(get_the_ID(), 'event_time', true); ?></h4>
-			            <p><?php echo get_post_meta(get_the_ID(), 'notes', true); ?></p>
-		            </div>
+<div class="sidebar">
+	<h2>Upcoming Events</h2>
+	<?php $query = new WP_Query( array(
+		                'post_type' => 'post',
+		                'category_name' => 'event',
+		                'order_by' => 'meta_value_num',
+		                'meta_key'  => 'event_date_info',
+		                'posts_per_page' => 3
+		            ) );
 		            
-	            
-	            <?php endwhile; 
-?>
+		          
+		            while ($query->have_posts()) : $query->the_post(); ?>
+			            <div class="home-update">
+				           
+				            <a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
+				            <h4><?php echo get_post_meta(get_the_ID(), 'event_date_info', true); ?>
+				            - <?php echo get_post_meta(get_the_ID(), 'event_time', true); ?></h4>
+				            <p><?php echo get_post_meta(get_the_ID(), 'notes', true); ?></p>
+			            </div>
+			            
+		            
+		            <?php endwhile; 
+	?>
+</div>
